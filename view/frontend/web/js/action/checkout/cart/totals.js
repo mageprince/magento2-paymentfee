0ec2894e1920540488683a6e41205886f0c3749e
Rémy LESCALLIER <remy.lescallier@smile.fr>
2021-03-15 14:29:26 +0100

    Add full screen loader and revert require Magento_SalesRule condition

--- view/frontend/web/js/action/checkout/cart/totals.js
+++ view/frontend/web/js/action/checkout/cart/totals.js
@@ -4,13 +4,16 @@ define([
     'Magento_Checkout/js/model/quote',
     'mage/storage',
     'mage/url',
-    'Magento_Checkout/js/action/get-totals'
-], function (ko, $, quote, storage, urlBuilder, getTotalsAction) {
+    'Magento_Checkout/js/action/get-totals',
+    'Magento_Checkout/js/model/full-screen-loader'
+], function (ko, $, quote, storage, urlBuilder, getTotalsAction, fullScreenLoader) {
         'use strict';
 
         return function (isLoading, payment) {
             let isEnabled = window.checkoutConfig.mageprince_paymentfee.isEnabled;
             if (isEnabled) {
+                fullScreenLoader.startLoader();
+
                 return storage.post(
                     urlBuilder.build('paymentfee/calculate/paymentfee'),
                     JSON.stringify({payment: payment})
@@ -23,6 +26,8 @@ define([
                     }
                 }).fail(function () {
                     isLoading(false);
+                }).always(function () {
+                    fullScreenLoader.stopLoader();
                 });
             }
         };
